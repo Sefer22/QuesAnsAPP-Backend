e5616bb43848f0a9e6f738a065d191503cb45b4f
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-10 01:23:39 +0400

    developed

---
+++ src/main/java/com/example/quesansappbackend/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.example.quesansappbackend.controller;
+
+import com.example.quesansappbackend.entity.User;
+import com.example.quesansappbackend.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class UserController {
+
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+       return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@PathVariable User user) {
+        return userRepository.save(user);
+    }
+}
