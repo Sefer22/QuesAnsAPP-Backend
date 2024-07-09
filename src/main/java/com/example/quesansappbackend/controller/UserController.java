c74797c59310d170af3d5ea1b3741d381df9154b
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-10 03:03:26 +0400

    developed

--- src/main/java/com/example/quesansappbackend/controller/UserController.java
+++ src/main/java/com/example/quesansappbackend/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.quesansappbackend.controller;
 
 import com.example.quesansappbackend.entity.User;
 import com.example.quesansappbackend.repository.UserRepository;
+import com.example.quesansappbackend.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -14,41 +15,36 @@ import java.util.Optional;
 @RequestMapping("/users")
 public class UserController {
 
-    private UserRepository userRepository;
+    private UserService userService;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping
     public List<User> getAllUsers() {
-       return userRepository.findAll();
+
+        return userService.getAllUsers();
     }
 
     @PostMapping
     public User createUser(@RequestBody User newUser) {
-        return userRepository.save(newUser);
+
+        return userService.saveOneUser(newUser);
     }
     @GetMapping("/{userId}")
     public User getOneUser(@PathVariable Long userId) {
         //custom exception
-        return userRepository.findById(userId).orElse(null);
+        return userService.getOneUser(userId);
     }
     @PutMapping("/{userId}")
     public User updateOneUser(@PathVariable Long userId,@RequestBody User newUser) {
-        Optional<User> user = userRepository.findById(userId);
-        if(user.isPresent()) {
-            User foundUser = user.get();
-            foundUser.setUserName(newUser.getUserName());
-            foundUser.setPassword(newUser.getPassword());
-            userRepository.save(foundUser);
-            return foundUser;
-        }else
-            return null;
+       return userService.updateOneUser(userId,newUser);
     }
     @DeleteMapping("/{userId}")
     public void deleteOneUser(@PathVariable Long userId) {
-        userRepository.deleteById(userId);
+
+        userService.deleteOneUser(userId);
     }
 
 
