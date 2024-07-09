3a610fcbace3e42a190667ca89ec029baf923cf9
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-10 01:35:09 +0400

    developed

--- src/main/java/com/example/quesansappbackend/controller/UserController.java
+++ src/main/java/com/example/quesansappbackend/controller/UserController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -24,13 +25,30 @@ public class UserController {
     }
 
     @PostMapping
-    public User createUser(@RequestBody User user) {
-        return userRepository.save(user);
+    public User createUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
     }
-
     @GetMapping("/{userId}")
     public User getOneUser(@PathVariable Long userId) {
         //custom exception
         return userRepository.findById(userId).orElse(null);
     }
+    @PutMapping("/{userId}")
+    public User updateOneUser(@PathVariable Long userId,@RequestBody User newUser) {
+        Optional<User> user = userRepository.findById(userId);
+        if(user.isPresent()) {
+            User foundUser = user.get();
+            foundUser.setUserName(newUser.getUserName());
+            foundUser.setPassword(newUser.getPassword());
+            userRepository.save(foundUser);
+            return foundUser;
+        }else
+            return null;
+    }
+    @DeleteMapping("/{userId}")
+    public void deleteOneUser(@PathVariable Long userId) {
+        userRepository.deleteById(userId);
+    }
+
+
 }
