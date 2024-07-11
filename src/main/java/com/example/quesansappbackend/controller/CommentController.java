10f5236459046d486a56432d56483ac83099bb70
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-12 01:24:54 +0400

    developed

--- src/main/java/com/example/quesansappbackend/controller/CommentController.java
+++ src/main/java/com/example/quesansappbackend/controller/CommentController.java
@@ -1,8 +1,14 @@
 package com.example.quesansappbackend.controller;
 
+import com.example.quesansappbackend.entity.Comment;
 import com.example.quesansappbackend.service.CommentService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/comments")
@@ -12,5 +18,10 @@ public class CommentController {
 
     public CommentController(CommentService commentService) {
         this.commentService = commentService;
+    }
+
+    @GetMapping
+    public List<Comment> getAllComments(@RequestParam Optional<Long> userId,@RequestParam Optional<Long> postId) {
+        return commentService.getAllCommentsWithParam(userId,postId);
     }
 }
--- src/main/java/com/example/quesansappbackend/service/CommentService.java
+++ src/main/java/com/example/quesansappbackend/service/CommentService.java
@@ -1,7 +1,11 @@
 package com.example.quesansappbackend.service;
 
+import com.example.quesansappbackend.entity.Comment;
 import com.example.quesansappbackend.repository.CommentRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CommentService {
@@ -10,5 +14,10 @@ public class CommentService {
 
     public CommentService(CommentRepository commentRepository) {
         this.commentRepository = commentRepository;
+    }
+
+    public List<Comment> getAllCommentsWithParam(Optional<Long> userId, Optional<Long> postId) {
+
+        return null;
     }
 }
