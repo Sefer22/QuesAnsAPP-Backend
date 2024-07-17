631efd3aca8b32f4889c71e15bdf5f6c33ee0e0b
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-18 00:52:48 +0400

    developed

--- src/main/java/com/example/quesansappbackend/config/SecurityConfig.java
+++ src/main/java/com/example/quesansappbackend/config/SecurityConfig.java
@@ -1,10 +1,19 @@
 package com.example.quesansappbackend.config;
 
 import com.example.quesansappbackend.security.JwtAuthenticationEntryPoint;
+import com.example.quesansappbackend.security.JwtAuthenticationFilter;
 import com.example.quesansappbackend.service.UserDetailsServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -17,5 +26,25 @@ public class SecurityConfig {
     public SecurityConfig(UserDetailsServiceImpl userDetailsService, JwtAuthenticationEntryPoint handler) {
         this.userDetailsService = userDetailsService;
         this.handler = handler;
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .csrf().disable()
     }
 }
