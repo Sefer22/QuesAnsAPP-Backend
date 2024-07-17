37e9d9c14aa16cf145be90bf83d8fa5947b1fa78
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-17 23:33:34 +0400

    developed

--- src/main/java/com/example/quesansappbackend/config/SecurityConfig.java
+++ src/main/java/com/example/quesansappbackend/config/SecurityConfig.java
@@ -1,4 +1,21 @@
 package com.example.quesansappbackend.config;
 
+import com.example.quesansappbackend.security.JwtAuthenticationEntryPoint;
+import com.example.quesansappbackend.service.UserDetailsServiceImpl;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private UserDetailsServiceImpl userDetailsService;
+
+    private JwtAuthenticationEntryPoint handler;
+
+    public SecurityConfig(UserDetailsServiceImpl userDetailsService, JwtAuthenticationEntryPoint handler) {
+        this.userDetailsService = userDetailsService;
+        this.handler = handler;
+    }
 }
