e63edd7c7c12f5263f8f0a09b2843c798b3775d5
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-17 21:20:22 +0400

    developed

---
+++ src/main/java/com/example/quesansappbackend/service/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.quesansappbackend.service;
+
+import com.example.quesansappbackend.repository.UserRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName();
+        return null;
+    }
+}
