dd8a39028a1c0b1fe0fdddf4e70798c7146cf8ce
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-17 21:16:30 +0400

    developed

--- src/main/java/com/example/quesansappbackend/security/JwtUserDetails.java
+++ src/main/java/com/example/quesansappbackend/security/JwtUserDetails.java
@@ -1,29 +1,36 @@
 package com.example.quesansappbackend.security;
 
+import com.example.quesansappbackend.entity.User;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
+@Getter
+@Setter
 public class JwtUserDetails implements UserDetails {
 
     public Long id;
     private String username;
     private String password;
     private Collection<? extends GrantedAuthority> authorities;
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+
+    public JwtUserDetails(Long id, String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
     }
 
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
+    public static JwtUserDetails create(User user) {
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        authorityList.add(new SimpleGrantedAuthority("user"));
+        return new JwtUserDetails(user.getId(), user.getUserName(), user.getPassword(),authorityList);
     }
 
     @Override
