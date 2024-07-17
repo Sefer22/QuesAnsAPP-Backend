3d58fbf86abeb5c13b9f90251bf3d2ea7129a68e
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-17 21:09:54 +0400

    developed

---
+++ src/main/java/com/example/quesansappbackend/security/JwtUserDetails.java
@@ -0,0 +1,48 @@
+package com.example.quesansappbackend.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class JwtUserDetails implements UserDetails {
+
+    public Long id;
+    private String username;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
