0ddbe6d45f25dc1f8644dfd4f849eebf37e77f1a
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-19 21:35:18 +0400

    developed

--- src/main/java/com/example/quesansappbackend/controller/AuthController.java
+++ src/main/java/com/example/quesansappbackend/controller/AuthController.java
@@ -1,7 +1,11 @@
 package com.example.quesansappbackend.controller;
 
 import com.example.quesansappbackend.request.UserRequest;
+import com.example.quesansappbackend.security.JwtTokenProvider;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
+    private AuthenticationManager authenticationManager;
+
+    private JwtTokenProvider jwtTokenProvider;
+
     @PostMapping("/login")
     public String login(@RequestBody UserRequest loginRequest) {
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(loginRequest.getUserName(),loginRequest.getPassword());
-
+        Authentication auth = authenticationManager.authenticate(authToken);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        String jwtToken
     }
 }
